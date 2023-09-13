cd0f8ac8764d3f6319f697ab112bfd2e747c01a1
Epou9000 <107133918+Epou9000@users.noreply.github.com>
2023-09-13 17:28:50 +0300

    Add files via upload
    *_*

---
+++ main.js
@@ -0,0 +1,48 @@
+let btn = document.querySelector('.burger');
+let menu = document.querySelector('.header-block');
+btn.addEventListener('click', function () {
+    menu.classList.toggle('open')
+
+})
+
+
+
+
+// let all= document.querySelector('.all');
+
+// all.addEventListener('click', function () {
+//    all.classList.toggle('close')
+
+
+
+// })
+
+let x = document.querySelector('.x')
+let x2 = document.querySelector('.x2')
+let x3= document.querySelector('.x3')
+
+
+
+btn.addEventListener('click', function () {
+    x.classList.toggle('close')
+ 
+ 
+ 
+ })
+
+
+
+ btn.addEventListener('click', function () {
+    x2.classList.toggle('close')
+ 
+ 
+ 
+ })
+ btn.addEventListener('click', function () {
+    x3.classList.toggle('close')
+ 
+ 
+ 
+ })
+
+
